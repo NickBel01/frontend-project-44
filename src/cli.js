05f7954a8c9013febfe6e4b14df44ff595066445
Nikita Belyaev <n.nikitin8888@gmail.com>
2025-12-13 18:08:00 +0300

    Install ESLint and fix all errors linter

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync'
 
 const helloUser = () => {
-    console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
-};
+  console.log('Welcome to the Brain Games!')
+  const name = readlineSync.question('May I have your name? ')
+  console.log(`Hello, ${name}!`)
+}
 
-export default helloUser;
+export default helloUser
